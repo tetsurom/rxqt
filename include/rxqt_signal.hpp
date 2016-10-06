53a4f631126bf9c183188a9f27e760c1f10f137e
tetsurom <matsumura.t.lab@gmail.com>
2016-10-07 01:04:56 +0900

    Refactor struct from_signal's template parameter

--- include/rxqt_signal.hpp
+++ include/rxqt_signal.hpp
@@ -12,13 +12,29 @@ namespace signal {
 
 namespace detail {
 
-template <class Signal>
+template <class R, class Q, class ...Args>
 struct from_signal
 {
+    using signal_type = R(Q::*)(const Args&...);
+    using value_type = std::tuple<Args...>;
+
+    static rxcpp::observable<value_type> create(const Q* qobject, signal_type signal)
+    {
+        return rxcpp::observable<>::create<value_type>(
+            [qobject, signal](const rxcpp::subscriber<value_type>& s){
+                QObject::connect(qobject, signal,[s](const Args&... values){
+                    s.on_next(std::make_tuple(values...));
+                });
+                QObject::connect(qobject, &QObject::destroyed, [s](){
+                    s.on_completed();
+                });
+            }
+        );
+    }
 };
 
 template <class R, class Q>
-struct from_signal<R(Q::*)()>
+struct from_signal<R, Q>
 {
     using signal_type = R(Q::*)();
     using value_type = long;
@@ -26,7 +42,7 @@ struct from_signal<R(Q::*)()>
     static rxcpp::observable<long> create(const Q* qobject, signal_type signal)
     {
         return rxcpp::observable<>::create<long>(
-            [qobject, signal](rxcpp::subscriber<long> s){
+            [qobject, signal](const rxcpp::subscriber<long>& s){
                 long counter = 0;
                 QObject::connect(qobject, signal, [s, counter]() mutable {
                     s.on_next(counter++);
@@ -40,7 +56,7 @@ struct from_signal<R(Q::*)()>
 };
 
 template <class R, class Q, class A0>
-struct from_signal<R(Q::*)(const A0&)>
+struct from_signal<R, Q, A0>
 {
     using signal_type = R(Q::*)(const A0&);
     using value_type = A0;
@@ -48,30 +64,9 @@ struct from_signal<R(Q::*)(const A0&)>
     static rxcpp::observable<value_type> create(const Q* qobject, signal_type signal)
     {
         return rxcpp::observable<>::create<value_type>(
-            [qobject, signal](rxcpp::subscriber<value_type> s){
+            [qobject, signal](const rxcpp::subscriber<value_type>& s){
                 QObject::connect(qobject, signal, [s](const A0& v0){
                     s.on_next(v0);
-                });
-                QObject::connect(qobject, &QObject::destroyed, [s](){
-                    s.on_completed();
-                });
-            }
-        );
-    }
-};
-
-template <class R, class Q, class A0, class ...Args>
-struct from_signal<R(Q::*)(const A0&, const Args&...)>
-{
-    using signal_type = R(Q::*)(const A0&, const Args&...);
-    using value_type = std::tuple<A0, Args...>;
-
-    static rxcpp::observable<value_type> create(const Q* qobject, signal_type signal)
-    {
-        return rxcpp::observable<>::create<value_type>(
-            [qobject, signal](rxcpp::subscriber<value_type> s){
-                QObject::connect(qobject, signal,[s](const A0& v0, const Args&... values){
-                    s.on_next(std::make_tuple(v0, values...));
                 });
                 QObject::connect(qobject, &QObject::destroyed, [s](){
                     s.on_completed();
@@ -85,11 +80,11 @@ struct from_signal<R(Q::*)(const A0&, const Args&...)>
 
 } // signal
 
-template <class Q, class Signal>
-rxcpp::observable<typename signal::detail::from_signal<Signal>::value_type>
-from_signal(Q* qobject, Signal signal)
+template <class R, class Q, class ...Args>
+rxcpp::observable<typename signal::detail::from_signal<R, Q, Args...>::value_type>
+from_signal(const Q* qobject, R(Q::*signal)(const Args&...))
 {
-    return signal::detail::from_signal<Signal>::create(qobject, signal);
+    return signal::detail::from_signal<R, Q, Args...>::create(qobject, signal);
 }
 
 } // qtrx
