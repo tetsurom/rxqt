c4dc39d7d62d7e4b63ac25562aa5fc7e1acf10d1
Stuart Dootson <stuart.dootson@gmail.com>
2017-01-25 17:10:27 +0000

    Add support for QPrivateSignal parameters
    1. They're only in as final parameters
    2. They're stripped out, as MOC does.

--- include/rxqt_signal.hpp
+++ include/rxqt_signal.hpp
@@ -82,15 +82,41 @@ struct from_signal<R, Q, A0>
     }
 };
 
+template <class Q, class T>
+struct is_private_signal : std::false_type {};
+
+template <class Q>
+struct is_private_signal<Q, typename Q::QPrivateSignal> : std::true_type {};
+
+template <class R, class Q, class T, class U>
+struct construct_signal_type;
+
+template <class R, class Q, class T, std::size_t... Is>
+struct construct_signal_type<R, Q, T, std::index_sequence<Is...>>
+{
+    using type = from_signal<R, Q, typename std::tuple_element<Is, T>::type...>;
+};
+
+template <class R, class Q, class ...Args>
+struct get_signal_factory
+{
+    using as_tuple = std::tuple<Args...>;
+    static constexpr bool has_private_signal =
+        is_private_signal<Q, typename std::tuple_element<sizeof...(Args) - 1, as_tuple>::type>::value;
+    static constexpr size_t arg_count = has_private_signal ? sizeof...(Args) - 1 : sizeof...(Args);
+    using type = typename construct_signal_type<R, Q, as_tuple, std::make_index_sequence<arg_count>>::type;
+};
+
 } // detail
 
 } // signal
 
 template <class R, class Q, class ...Args>
-rxcpp::observable<typename signal::detail::from_signal<R, Q, Args...>::value_type>
+rxcpp::observable<typename signal::detail::get_signal_factory<R, Q, Args...>::type::value_type>
 from_signal(const Q* qobject, R(Q::*signal)(Args...))
 {
-    return signal::detail::from_signal<R, Q, Args...>::create(qobject, signal);
+    using signal_factory = typename signal::detail::get_signal_factory<R, Q, Args...>::type;
+    return typename signal_factory::create(qobject, reinterpret_cast<typename signal_factory::signal_type>(signal));
 }
 
 } // qtrx
